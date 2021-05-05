49a93e70969b95bd82f7ae2ce82580516785985b
komalbo10 <kbkomalbonde@gmail.com>
2021-05-05 10:19:00 +0100

    Add files via upload

---
+++ GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebAppCalculator
+{
+    public class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("https://localhost:44385/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
